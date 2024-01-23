0376565948ef04eb7457317e39b236ea8014bd35
Kascald <kghui20@gmail.com>
2024-01-24 00:21:45 +0900

    before merge develop

--- src/POS/DAO.java
+++ src/POS/DAO.java
@@ -1,5 +1,10 @@
 package POS;
 
 public class DAO {
+    //before merge develop3 into develop
+    //develop3를 -->> develop(전체 개발 브랜치)에 병합(merge)하기전 추가하는 merge용 커밋
 
+    //develop 1, 2, 3, 4는 develop 브랜치를 base로 한다
+
+    //develop 브랜치는 main을 base로 한다
 }
